3a46e9bb40ae494e9746c64f0b85d8188b15c4e5
Mevaser Zehoray <Mevaserruppin@gmail.com>
2025-02-20 11:16:00 +0200

    add comments

--- createAccounts.py
+++ createAccounts.py
@@ -4,12 +4,14 @@ def main():
     fake = Faker()
 
     list_of_emails = []
+    #create 2000 fake emails with @gmail.com domain
     for i in range(2000):
         user_part = fake.user_name()
         custom_email = user_part + "@gmail.com"
         list_of_emails.append(custom_email)
         print(list_of_emails[i])
 
+    #write the emails to a file
     with open("FakedEmail.txt", "w", encoding='utf-8') as file:
         for email in list_of_emails:
             file.write(email + "\n")
